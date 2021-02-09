5f4c96d00bfb3f9bcd869bff58a296e8030f3e87
Wilhelm Berghammer <wilhelm@berghammer.io>
2021-02-09 13:21:14 +0100

    playing around with models - next: design database

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,8 @@
+'''
+	TODO:
+		* design database
+'''
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -10,6 +15,9 @@ class Stock(models.Model):
 	symbol = models.CharField(max_length=10)
 	start_date = models.CharField(max_length=20) 	# example: 2020-03-23 09:00:00
 	amount = models.IntegerField(default=1)
+	starting_price = models.DecimalField(max_digits=100, decimal_places=2, default=0)
+	current_prince = models.DecimalField(max_digits=100, decimal_places=2, default=0)
+
 
 	def __str__(self):
 		return f'{self.symbol}, bought {self.amount} shares on {self.start_date}'
--- portfolio/views.py
+++ portfolio/views.py
@@ -8,16 +8,34 @@ TODO:
 		* calculate ytd performance
 		* calculate overall performance
 		* graph performance
+	* add position
 '''
 
-
+import requests
+import os 
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Stock
 
 
+def get_data(symbols, interval, start_date):
+	'''
+		* symbols ... stock symbols -> list
+		* interval ... interval -> sting (1min, 5min, 15min, 30min, 45min, 1h, 2h, 4h, 8h, 1day, 1week, 1month)
+		* start_date ... start date -> YYYY-MM-DD
+	'''
+	api_key = os.environ.get("TWELVE_DATA_API_KEY")
+
+
+	requests = response = requests.get(f"https://api.twelvedata.com/time_series?symbol={symbols}&interval={interval}&start_date={start_date}&apikey={api_key}")
+
+
 def portfolio_list(request):
 	stocks = Stock.objects.all()
+
+
+
 
 	context = {
 		"stocks": stocks
