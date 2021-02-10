6c3514cf068ca6f72ec15c4fe6ab4f33639c1ec9
Wilhelm Berghammer <wilhelm@berghammer.io>
2021-02-10 19:29:35 +0100

    portfolio models

--- portfolio/views.py
+++ portfolio/views.py
@@ -19,22 +19,21 @@ from django.views import generic
 from .models import Stock
 
 
-def get_data(symbols, interval, start_date):
+def get_data(symbol, interval, start_date):
 	'''
-		* symbols ... stock symbols -> list
+		* symbol ... stock symbol -> string
 		* interval ... interval -> sting (1min, 5min, 15min, 30min, 45min, 1h, 2h, 4h, 8h, 1day, 1week, 1month)
 		* start_date ... start date -> YYYY-MM-DD
 	'''
 	api_key = os.environ.get("TWELVE_DATA_API_KEY")
 
 
-	requests = response = requests.get(f"https://api.twelvedata.com/time_series?symbol={symbols}&interval={interval}&start_date={start_date}&apikey={api_key}")
+	response = requests.get(f"https://api.twelvedata.com/time_series?symbol={symbol}&interval={interval}&start_date={start_date}&apikey={api_key}")
+
 
 
 def portfolio_list(request):
 	stocks = Stock.objects.all()
-
-
 
 
 	context = {
