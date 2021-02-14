aabad36338a756c785a88998fa093bac418c57cf
Wilhelm Berghammer <wilhelm@berghammer.io>
2021-02-14 13:48:38 +0100

    update view

--- portfolio/views.py
+++ portfolio/views.py
@@ -8,18 +8,6 @@ from .forms import PositionForm, StockModelForm
 
 
 
-def get_data(symbol, interval, start_date):
-	'''
-		* symbol ... stock symbol -> string
-		* interval ... interval -> sting (1min, 5min, 15min, 30min, 45min, 1h, 2h, 4h, 8h, 1day, 1week, 1month)
-		* start_date ... start date -> YYYY-MM-DD
-	'''
-	api_key = os.environ.get("TWELVE_DATA_API_KEY")
-
-
-	response = requests.get(f"https://api.twelvedata.com/time_series?symbol={symbol}&interval={interval}&start_date={start_date}&apikey={api_key}")
-
-
 
 def portfolio_list(request):
 	stocks = Stock.objects.all()
@@ -28,9 +16,6 @@ def portfolio_list(request):
 		"stocks": stocks
 	}
 	return render(request, "portfolio/portfolio_list.html", context)
-
-
-
 
 
 
@@ -88,8 +73,6 @@ def PositionDetailView(request, pk):
 		'change_color': change_color,
 	}	
 	return render(request, "portfolio/detail_position.html", context)
-
-
 
 
 
